021339bb923da3444c4d21df5c896302945f5a5b
bvasilop <bvasilop@gmail.com>
2019-04-18 20:09:14 -0700

    updated blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,7 +6,7 @@ const BlogPage = () => {
     return (
         <div>
             <h1>Blog</h1>
-            <p>Posts will show up here later on.</p>
+            <p>Posts will show up here later.</p>
         </div>
     )
 };
