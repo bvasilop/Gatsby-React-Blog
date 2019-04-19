004ef85cfc3958fea5b6a95a18126d741d8adbd1
bvasilop <bvasilop@gmail.com>
2019-04-18 20:00:05 -0700

    updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => {
         <div>
             <h1>Hello.</h1>
             <h2>I'm Bill, a front-end engineer living in beautiful Seattle</h2>
+            <p>Need a developer? <a href="/contact">Contact Me</a></p>
         </div>
     )
 };
