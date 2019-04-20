98a561335a2d8e62ac59a9ab31ccfdfe811c673d
bvasilop <bvasilop@gmail.com>
2019-04-19 19:28:20 -0700

    added and configured gatsby file source plugin system

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
         author: 'Bill Vasilopoulos'
     },
     plugins: [
-        'gatsby-plugin-sass'
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`
+            }
+        }
     ]
 };
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "Setting Up Gatsby Blog Post"
+date: "2019-04-04"
+---
+
+I just added my first blog post using Gatsby!
+
+## Topics covered
+
+1. Gatsby
+2. GraphQl
+3. React
