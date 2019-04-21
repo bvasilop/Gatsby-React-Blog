44f4451f1bf7e5e1c27c34497e0a8de8d065f5fc
bvasilop <bvasilop@gmail.com>
2019-04-21 13:00:46 -0700

    swapped out md files for contentful config settings

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
-import {  Link, graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import blogStyles from './blog.module.scss';
 
 /* eslint-disable */
@@ -8,32 +8,33 @@ import blogStyles from './blog.module.scss';
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
     query {
-        allMarkdownRemark {
-            edges {
-                node {
-                    frontmatter {
+        allContentfulBlogPost (
+            sort: {
+                fields: publishedDate,
+                order: DESC
+            }
+            ){
+                edges {
+                    node {
                         title
-                        date
-                    }
-                    fields {
                         slug
+                        publishedDate(formatString:"MMMM Do, YYYY")
                     }
                 }
             }
         }
-    }
 `)
     return (
         <div>
             <Layout>
             <h1>Blog</h1>
                 <ol className={blogStyles.posts}>
-                        {data.allMarkdownRemark.edges.map((edge) => {
+                        {data.allContentfulBlogPost.edges.map((edge) => {
                             return (
                                 <li className={blogStyles.post}>
-                                    <Link to={`/blog/${edge.node.fields.slug}`}>
-                                        <h2>{edge.node.frontmatter.title}</h2>
-                                        <p>{edge.node.frontmatter.date}</p>
+                                    <Link to={`/blog/${edge.node.slug}`}>
+                                        <h2>{edge.node.title}</h2>
+                                        <p>{edge.node.publishedDate}</p>
                                     </Link>
                                 </li>
                             )
